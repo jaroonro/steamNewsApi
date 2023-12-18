85943fcd325c34044590a1a8cd8b0a0d211ce3d7
jaroonro <charoonroj.amo@gmail.com>
2023-12-18 23:08:33 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: '*',
+  methods: 'GET',
+  credentials: true,
+}));
+
+app.use(express.static('public'));
+
+app.get('/api/news', async (req, res) => {
+  const { appId } = req.query;
+
+  if (!appId) {
+    return res.status(400).json({ error: 'Missing appId in the request' });
+  }
+
+  try {
+    console.log('Before fetch');
+    const { default: fetch } = await import('node-fetch');
+    const response = await fetch(`https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=${appId}&count=31&maxlength=300&format=json`);
+    console.log('After fetch');
+
+    if (!response.ok) {
+      console.error('Error fetching news. Response:', await response.text());
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching news:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
